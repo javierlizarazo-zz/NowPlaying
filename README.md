993b850d82439c76dd6b0ea740c76e8073959f97
javierlizarazo <javilizarazo@gmail.com>
2014-10-24 02:12:51 -0500

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 NowPlaying
 ==========
+
+Tools used:
+
+jQuery /
+jQuery Template /
+Codebird /
+Css via less
+
+/***********************/
+
+jQuery Template:
+
+	- Used to load each one of the tweets, I like to use this library because I think is cleaner than create an append in the scripts file to load all the content pulled from de Twitter service
+
+Codebird
+
+	- Is the first time I use this one, but I think it was really easy and helpful in order to authenticate the Twitter app and to call the service.
+
+Less
+
+	- I like to use less in my projects since it is a great way to write css with variables so you can reuse code and don't need to write the same thing over and over and over.
--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,10 @@
 $(document).on('ready', function(){
-    //getLocation();
-    getTweets();
+    getLocation();
+    //validateApi();
+
+    $('.tweetBtn').on('click', function(){
+        tweetNew();
+    });
 });
 // END Doc Ready
 
@@ -17,13 +21,17 @@ function showPosition(position) {
     console.log(position);
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-    //getTweets();
+    validateApi();
 }
 
-function getTweets() {
-    var cb = new Codebird;
-    cb.setConsumerKey("k2Pj4kyLrLMI9bozMxGyejM7M", "u0SQcO2L7wQmXaPJ7I7a6jusokIVDeQPfWCbX1pRiXnG628Xbd");
-    // cb.setToken("127322873-DDEI4KMW0mEiEC9FapYj74I5OfgWSZJ8OmTwLjEL", "QJNwyQBgPpAroNUZtepDu6eCA7Y7lfz8LmueW9CX5Losa");
+// setInterval(function(){
+//     getTweets();
+// }, 10000);
+
+var cb = new Codebird;
+
+function validateApi(){
+    cb.setConsumerKey("gEFEbGdkVTDfzVgyiiCbzUImi", "Q4yQ000AiJL110zoNEkBYL0ASl84SUcnaxkCJ01uZzeghqWeXX");
     cb.__call(
         "oauth2_token",
         {},
@@ -31,16 +39,24 @@ function getTweets() {
             var bearer_token = reply.access_token;
         }
     );
-    var token = "AAAAAAAAAAAAAAAAAAAAADJQbAAAAAAAqMcYtU7yxTKUag2ab0oBaXAPbPM%3D0yRYYB7dhbIlpCKraGo2FrYrFq5u3wTK1n1nkS8pmeBsk82hJV";
+    var token = "2BvWRRiTPyQUiMU2Zl59yOdfk%3DECQQdP5dVANyvH62RNhHioT4culhhqLhCTE5TsAMhqbd39HI1Z";
     cb.setBearerToken(token);
+    getTweets();
+}
 
+function getTweets() {
+    $('.loader').show();
+    
     geoCode = [latitude, longitude, '1mi'];
     var params = {
         q: "NowPlaying",
-        count: '5',
+        count: '1000',
         result_type: 'recent'
-        //geocode: geoCode.join()
+        //,geocode: geoCode.join()
     };
+
+    var nplayer = 0;
+
     cb.__call(
         "search_tweets",
         params,
@@ -55,17 +71,75 @@ function getTweets() {
                     name: eachTweet[i].user.name,
                     user: '@' + eachTweet[i].user.screen_name,
                     text: eachTweet[i].text,
-                    follow: 'https://twitter.com/intent/retweet?tweet_id=' + eachTweet[i].id
+                    retweet: 'https://twitter.com/intent/retweet?tweet_id=' + eachTweet[i].id_str,
+                    reply: 'https://twitter.com/intent/tweet?in_reply_to=' + eachTweet[i].id_str,
+                    favorite: 'https://twitter.com/intent/favorite?tweet_id=' + eachTweet[i].id_str,
+                    follow: 'https://twitter.com/intent/user?screen_name=' + eachTweet[i].user.screen_name
                 }
-                elemeObj.push(tweetInfo);
+
+
+                $.each(eachTweet[i].entities.urls, function(index, element){
+                    if(element.expanded_url.indexOf('youtu') != -1 && nplayer < 5){
+                        console.log(element.expanded_url);
+                        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+                        var match = element.expanded_url.match(regExp);
+                        if (match && match[7].length == 11){
+                            tweetInfo.video = match[7];
+                            tweetInfo.playerId = 'playerId' + nplayer;
+                            elemeObj.push(tweetInfo);
+                        }
+                        nplayer++;
+                    }
+                });
             }
             $('.tweetContainer').loadTemplate('#tweetsTemplate', elemeObj);
-
-            $('.tweetWidget .button a').attr('target', '_blank');
+            $('.loader').hide();
+            onAPIReady();
         }
     );
 }
 
+function tweetNew() {
+    var url = window.location.href;
+    var text = $('#Comment').val();
+    var link = $('#videoUrl').val();
+    var hash = "NowPlaying";
+
+    window.open('https://twitter.com/intent/tweet?url='+ url +'&text='+ text + link +'&hashtags='+ hash +'', 'twitter', 'toolbar=no,width=514, height=414');
+    return false;
+}
+
+
+/****************************************** YOUTUBE PLAYER */
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var playerPos = new Array();
+
+function onAPIReady() {
+    if($('.video').length > 0){
+        $.each($('.video'), function(i, e){
+            var videoID = $(e).attr('alt');
+            var id = $(e).attr('id');
+
+            player = new YT.Player(id, {
+                videoId: videoID,
+                playerVars: {
+                    html5: 1,
+                    loop: 1,
+                    wmode: "opaque"
+                }
+            });
+
+            playerPos[i] = player;          
+        });
+    }
+
+}
 
 
 
